92b9c7bb196c5a6a6c5718dcb2f8f571148c6768
Bozhidar Hristov <b.hristov@vm5.eu>
2017-06-12 18:15:34 +0300

    Add address validation

--- src/RequestBuilder/ServiceRequestBuilder.php
+++ src/RequestBuilder/ServiceRequestBuilder.php
@@ -31,6 +31,24 @@ class ServiceRequestBuilder extends AbstractRequestBuilder
         $xml = new \SimpleXMLElement('<?xml version="1.0" encoding="utf-8"?><request/>');
         $xml->addChild('request_type', $this->getRequestType($request));
 
+        if ($request instanceof AddressValidationRequest) {
+            $address = $xml->addChild('address');
+
+            $address->addChild('city', $request->getAddress()->getCity());
+            $address->addChild('post_code', $request->getAddress()->getPostCode());
+            $address->addChild('quarter', $request->getAddress()->getQuarter());
+
+            if ($request->getAddress()->getStreet()) {
+                $address->addChild('street', $request->getAddress()->getStreet()->getName());
+                $address->addChild('street_num', $request->getAddress()->getStreet()->getNumber());
+                $address->addChild('street_bl', $request->getAddress()->getStreet()->getBlock());
+                $address->addChild('street_vh', $request->getAddress()->getStreet()->getEntrance());
+                $address->addChild('street_et', $request->getAddress()->getStreet()->getFloor());
+                $address->addChild('street_ap', $request->getAddress()->getStreet()->getApartment());
+                $address->addChild('street_other', $request->getAddress()->getStreet()->getOther());
+            }
+        }
+
         return $xml;
     }
 }
